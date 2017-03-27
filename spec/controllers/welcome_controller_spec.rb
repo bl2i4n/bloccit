5d187050f916fcec771c348c63dcb49254c2f50d
bl2i4n <bl2i4n@yahoo.com>
2017-03-26 23:18:46 -0700

    added faq test, route, html file and all tests pass

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+        expect(response).to render_template("faq")
+    end
+  end
+
 end
