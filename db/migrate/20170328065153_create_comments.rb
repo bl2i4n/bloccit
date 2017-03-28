02b9012773df186025e1746aebe91328393e87ce
bl2i4n <bl2i4n@yahoo.com>
2017-03-28 00:15:13 -0700

    need to finish the last model(update) in chkpt15

--- db/migrate/20170328065153_create_comments.rb
+++ db/migrate/20170328065153_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
+
       t.references :post, index: true, foreign_key: true
 
       t.timestamps null: false
