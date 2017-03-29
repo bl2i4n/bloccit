41b4a469835ac71bf908fae3d63f78e5051a155c
bl2i4n <bl2i4n@yahoo.com>
2017-03-29 01:10:51 -0700

    end of assignment 15 w/o looking at solution, added specs for q&a

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) {Question.create!(title: "New Post Title", body: "New Post Body", resolved: boolean)}
+  let(:answer) {Answer.create!(body: 'Comment Body', question: question)}
+
+  describe "attributes" do
+    it "has a body attribute and references question" do
+      expect(answer).to have_attributes(body: "Comment Body")
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) {Question.create!(title: "New Question Title", body: "New Post Body", resolved: boolean)}
+
+  describe "attributes" do
+    it "has a body attribute" do
+      expect(question).to have_attributes(body: "Comment Body")
+    end
+
+    it "has a boolean attribute" do
+      expect(question).to have_attributes(resolved: boolean)
+    end
+
+  end
 end
