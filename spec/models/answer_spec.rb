fb2bf68ef98dc3124310e66938c40384e0fa241f
bl2i4n <bl2i4n@yahoo.com>
2017-03-29 22:16:33 -0700

    finished assignment15 in checkpoint15, wrote specs for answer and question, tests pass

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) {Question.new(title: "New Post Title", body: "New Post Body", resolved: false)}
+  let(:answer) {Answer.new(question: question, body: 'Answer Body')}
+
+    it "should respond to body" do
+      expect(answer).to respond_to(:body)
+    end
+
+end
