71136f1cb8a86b6d65d55c70bfe58f443d0b808e
bl2i4n <bl2i4n@yahoo.com>
2017-04-02 23:08:16 -0700

    iterate through posts and every 5th post change title to spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def index
     #11
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
