12a23ae3f11a072e5da7a7b268198724ab16cc4c
bl2i4n <bl2i4n@yahoo.com>
2017-04-05 20:11:28 -0700

    fixed assignment18 issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @posts = Post.all
     @posts.each_with_index do |post, index|
       if index % 5 == 0
-        post.title = "SPAM"
+        post.update(title: "SPAM")
       end
     end
   end
