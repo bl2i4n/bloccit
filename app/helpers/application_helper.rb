e7bf48dd05f201187915905733fbe64468588040
bl2i4n <bl2i4n@yahoo.com>
2017-04-11 20:45:42 -0700

    end of checkpoint23, was able to see validation errors for title and body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #4
+  def form_group_tag(errors, &block)
+    css_class = 'form-group'
+    css_class << ' has error' if errors.any?
+
+    #5
+    content_tag :div, capture(&block), class: css_class
+  end
 end
