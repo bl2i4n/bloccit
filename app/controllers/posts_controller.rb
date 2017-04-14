6abf992d5979518faa28cc6b154bc021f88049a3
bl2i4n <bl2i4n@yahoo.com>
2017-04-13 17:37:54 -0700

    reviewing assignment18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,59 @@ class PostsController < ApplicationController
   end
 
   def show
+    #19
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    #9
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    #10
+    if @post.save
+      #11
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      #12
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving post. Please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    # 8
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
   end
 end
