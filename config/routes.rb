557cea1d65a419347b90f300ff198d188fde132e
bl2i4n <bl2i4n@yahoo.com>
2017-04-18 00:44:13 -0700

    completed: user sign up form and confirmation view

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   #resource routing instead of multiple routes
-  resources :advertisement
+  # resources :advertisement
+  post 'users/confirm' => 'users#confirm'
 
-
+  resources :users
   #2
   get 'about' => 'welcome#about'
 
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  #1
+  let(:new_user_attributes) do
+    {
+      name: "Blochead",
+      email: "blochead@bloc.io",
+      password: "blochead",
+      password_confirmation: "blochead"
+    }
+  end
+
+  #2
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "instantiates a new user" do
+      get :new
+      expect(assigns(:user)).to_not be_nil
+    end
+  end
+
+  #3
+  describe "POST create" do
+    it "returns an http redirect" do
+      post :create, user: new_user_attributes
+      expect(response).to have_http_status(:redirect)
+    end
+
+    #4
+    it "creates a new user" do
+      expect{
+        post :create, user: new_user_attributes
+      }.to change(User, :count).by(1)
+    end
+
+    #5
+    it "sets user name properly" do
+      post :create, user: new_user_attributes
+      expect(assigns(:user).name).to eq new_user_attributes[:name]
+    end
+
+    #6
+    it "sets user email properly" do
+      post :create, user: new_user_attributes
+      expect(assigns(:user).email).to eq new_user_attributes[:email]
+    end
+
+    #7
+    it "sets user password properly" do
+      post :create, user: new_user_attributes
+      expect(assigns(:user).password).to eq new_user_attributes[:password]
+    end
+
+    #8
+    it "sets user password_confirmation properly" do
+      post :create, user: new_user_attributes
+      expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+    end
+
+  end
+
+end
