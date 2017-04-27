93136aeb5ec1554ca4e5a600f01fe42aaf2aa1ce
bl2i4n <bl2i4n@yahoo.com>
2017-04-26 20:32:02 -0700

    fixing topic id attribute err

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412044819) do
+ActiveRecord::Schema.define(version: 20170421051503) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -36,9 +36,11 @@ ActiveRecord::Schema.define(version: 20170412044819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +56,15 @@ ActiveRecord::Schema.define(version: 20170412044819) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
