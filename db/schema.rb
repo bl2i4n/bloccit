cc20ef83c71c5e88decfdf782a9160a3ce5fb386
bl2i4n <bl2i4n@yahoo.com>
2017-04-26 19:58:10 -0700

    fixing view errors as mod

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20170421051503) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
