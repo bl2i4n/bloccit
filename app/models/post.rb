64916f95e3ad25dd49f87a6504f0eb443dbe269e
bl2i4n <bl2i4n@yahoo.com>
2017-04-28 20:03:01 -0700

    end of chkpt and assignment, votes displayed in correct order and creating a post gets upvoted automagically

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
   default_scope { order('rank DESC')}
 
   validates :title, length: {minimum: 5}, presence: true
@@ -27,6 +28,12 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 
 end
